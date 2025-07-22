30045ce83e8fdefc2e45e33192af8152860cb72a
Kudaibergen <kudash1525@gmail.com>
2025-07-22 23:44:56 +0600

    Add new SVG icons and remove outdated PNG icons

--- src/assets/AllExportIcon.js
+++ src/assets/AllExportIcon.js
@@ -1,47 +1,52 @@
-import ArrowsDownIcon from './images/icon/arrows/Arrows-Down.png'
-import ArrowsLeftIcon from './images/icon/arrows/Arrows-Left.png'
-import ArrowsRightIcon from './images/icon/arrows/Arrows-Right.png'
-import ArrowsUpIcon from './images/icon/arrows/Arrows-Up.png'
-import FilesAndFoldersIcon from './images/icon/side panel/Side panel,State=Files and Folders Icon.svg'
-import GraphicIcon from './images/icon/side panel/Side panel,State=Graphic Icon.png'
-import LayoutIcon from './images/icon/side panel/Side panel,State=Layout Icon.svg'
-import PeopleIcon from './images/icon/side panel/Side panel,State=People Icon.png'
-import ToolsIcon from './images/icon/side panel/Side panel,State=Tools Icon.png'
-import ArchiveIcon from './images/icon/system/System,State=Archive.svg'
-import ArrowsSystemIcon from './images/icon/system/System,State=Arrows Icon.png'
-import AttachIcon from './images/icon/system/System,State=Attach.png'
-import CalendarIcon from './images/icon/system/System,State=Calendar.svg'
-import CheckIcon from './images/icon/system/System,State=Check.png'
-import ClockIcon from './images/icon/system/System,State=Clock.png'
-import CopyIcon from './images/icon/system/System,State=Copy.svg'
-import DeleteIcon from './images/icon/system/System,State=Delete.png'
-import DoneIcon from './images/icon/system/System,State=Done.svg'
-import EditIcon from './images/icon/system/System,State=Edit.png'
-import ExitIcon from './images/icon/system/System,State=Exit.png'
-import HideIcon from './images/icon/system/System,State=Hide.svg'
-import LabelIcon from './images/icon/system/System,State=Label.svg'
-import MemberIcon from './images/icon/system/System,State=Member.png'
-import MinusIcon from './images/icon/system/System,State=Minus.png'
-import NotificationIcon from './images/icon/system/System,State=Notification.png'
-import NotifyIcon from './images/icon/system/System,State=Notify.svg'
-import PlusIcon from './images/icon/system/System,State=Plus.png'
-import SearchIcon from './images/icon/system/System,State=Search.png'
-import ShowIcon from './images/icon/system/System,State=Show.png'
-import StarIcon from './images/icon/system/System,State=Star.png'
-import TemplateIcon from './images/icon/system/System,State=Template.svg'
+//? arrows
+import LeftIcon from './images/icon/arrows/left.svg?react'
+import RightIcon from './images/icon/arrows/right.svg?react'
+import UpIcon from './images/icon/arrows/up.svg?react'
+import DownIcon from './images/icon/arrows/down.svg?react'
+//? side panel
+import FilesAndFoldersIcon from './images/icon/side panel/Files and Folders Icon.svg?react'
+import GraphicIcon from './images/icon/side panel/Graphic Icon.svg?react'
+import LayoutIcon from './images/icon/side panel/Layout Icon.svg?react'
+import PeopleIcon from './images/icon/side panel/People Icon.svg?react'
+import ToolsIcon from './images/icon/side panel/Tools Icon.svg?react'
+//? system
+import ArchiveIcon from './images/icon/system/Archive.svg?react'
+import ArrowsIcon from './images/icon/system/Arrows Icon.svg?react'
+import AttachIcon from './images/icon/system/Attach.svg?react'
+import CalendarIcon from './images/icon/system/Calendar.svg?react'
+import CheckIcon from './images/icon/system/Check.svg?react'
+import ClockIcon from './images/icon/system/Clock.svg?react'
+import CopyIcon from './images/icon/system/Copy.svg?react'
+import DeleteIcon from './images/icon/system/Delete.svg?react'
+import DoneIcon from './images/icon/system/Done.svg?react'
+import EditIcon from './images/icon/system/Edit.svg?react'
+import ExitIcon from './images/icon/system/Exit.svg?react'
+import HideIcon from './images/icon/system/Hide.svg?react'
+import LabelIcon from './images/icon/system/Label.svg?react'
+import MemberIcon from './images/icon/system/Member.svg?react'
+import MinusIcon from './images/icon/system/Minus.svg?react'
+import NotificationIcon from './images/icon/system/Notification.svg?react'
+import PlusIcon from './images/icon/system/Plus.svg?react'
+import SearchIcon from './images/icon/system/Search.svg?react'
+import ShowIcon from './images/icon/system/Show.svg?react'
+import StarIcon from './images/icon/system/Star.svg?react'
+import TemplateIcon from './images/icon/system/Template.svg?react'
 
-export const AllExportIcon = {
-   ArrowsDownIcon,
-   ArrowsLeftIcon,
-   ArrowsRightIcon,
-   ArrowsUpIcon,
+export {
+   //? arrows
+   LeftIcon,
+   RightIcon,
+   UpIcon,
+   DownIcon,
+   //? side panel
    FilesAndFoldersIcon,
    GraphicIcon,
    LayoutIcon,
    PeopleIcon,
    ToolsIcon,
+   //? system
    ArchiveIcon,
-   ArrowsSystemIcon,
+   ArrowsIcon,
    AttachIcon,
    CalendarIcon,
    CheckIcon,
@@ -56,7 +61,6 @@ export const AllExportIcon = {
    MemberIcon,
    MinusIcon,
    NotificationIcon,
-   NotifyIcon,
    PlusIcon,
    SearchIcon,
    ShowIcon,
