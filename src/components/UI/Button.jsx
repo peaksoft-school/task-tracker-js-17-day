8c5259ce9f4db46ec10537f89c1c9ca3ccce870a
Temirlan Bakirov <temirlanbakirov10@gmail.com>
2025-07-22 19:27:43 +0600

    finich color

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-export const Button = ({children}) => {
-  return (
-    <div>Button</div>
-  )
+export const Button = ({ children }) => {
+   return <div>Button</div>
 }
