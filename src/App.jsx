13e90da034905738901b0c1f748fbf1d71095a6d
Temirlan Bakirov <temirlanbakirov10@gmail.com>
2025-07-22 19:42:57 +0600

    finich colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { themeColors } from './styles/Clors'
-
 function App() {
    return (
       <>
-         <div color={themeColors.palette.primary.main}>
-            <h1>Hlowoo</h1>
-            <h1>Hlowoo</h1>
-         </div>
+         <div></div>
       </>
    )
 }
--- src/components/UI/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Button = ({ children }) => {
-   return <div>Button</div>
-}
