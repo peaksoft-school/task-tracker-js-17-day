4c127209dafa99ec42c54de0870403bec2c05370
Temirlan Bakirov <temirlanbakirov10@gmail.com>
2025-07-22 19:17:39 +0600

    first colors

---
+++ src/styles/Clors.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material'
+
+export const themeColors = createTheme({
+   palette: {
+      primary: {
+         white: '#FFFFFF',
+         black: '#111111',
+         blue: '#0079BF',
+      },
+      secondary: {
+         grey: '#919191',
+         grey2: '#B2B2B2',
+         lightGrey: '#F0F0F0',
+      },
+   teriary: {
+         red: '#D91212',
+         green: '#2CB107',
+         litelgreen: '#66C74B',
+      },
+   },
+})
