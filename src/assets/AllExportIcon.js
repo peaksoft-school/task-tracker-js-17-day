91e787279712ebc2256560a83bc6ce8538c20e92
Kudaibergen <kudash1525@gmail.com>
2025-07-22 15:23:26 +0600

    add file AddExportIcon

---
+++ src/assets/AllExportIcon.js
@@ -0,0 +1,65 @@
+import ArrowsDownIcon from './images/icon/arrows/Arrows-Down.png'
+import ArrowsLeftIcon from './images/icon/arrows/Arrows-Left.png'
+import ArrowsRightIcon from './images/icon/arrows/Arrows-Right.png'
+import ArrowsUpIcon from './images/icon/arrows/Arrows-Up.png'
+import FilesAndFoldersIcon from './images/icon/side panel/Side panel,State=Files and Folders Icon.svg'
+import GraphicIcon from './images/icon/side panel/Side panel,State=Graphic Icon.png'
+import LayoutIcon from './images/icon/side panel/Side panel,State=Layout Icon.svg'
+import PeopleIcon from './images/icon/side panel/Side panel,State=People Icon.png'
+import ToolsIcon from './images/icon/side panel/Side panel,State=Tools Icon.png'
+import ArchiveIcon from './images/icon/system/System,State=Archive.svg'
+import ArrowsSystemIcon from './images/icon/system/System,State=Arrows Icon.png'
+import AttachIcon from './images/icon/system/System,State=Attach.png'
+import CalendarIcon from './images/icon/system/System,State=Calendar.svg'
+import CheckIcon from './images/icon/system/System,State=Check.png'
+import ClockIcon from './images/icon/system/System,State=Clock.png'
+import CopyIcon from './images/icon/system/System,State=Copy.svg'
+import DeleteIcon from './images/icon/system/System,State=Delete.png'
+import DoneIcon from './images/icon/system/System,State=Done.svg'
+import EditIcon from './images/icon/system/System,State=Edit.png'
+import ExitIcon from './images/icon/system/System,State=Exit.png'
+import HideIcon from './images/icon/system/System,State=Hide.svg'
+import LabelIcon from './images/icon/system/System,State=Label.svg'
+import MemberIcon from './images/icon/system/System,State=Member.png'
+import MinusIcon from './images/icon/system/System,State=Minus.png'
+import NotificationIcon from './images/icon/system/System,State=Notification.png'
+import NotifyIcon from './images/icon/system/System,State=Notify.svg'
+import PlusIcon from './images/icon/system/System,State=Plus.png'
+import SearchIcon from './images/icon/system/System,State=Search.png'
+import ShowIcon from './images/icon/system/System,State=Show.png'
+import StarIcon from './images/icon/system/System,State=Star.png'
+import TemplateIcon from './images/icon/system/System,State=Template.svg'
+
+export const AllExportIcon = {
+   ArrowsDownIcon,
+   ArrowsLeftIcon,
+   ArrowsRightIcon,
+   ArrowsUpIcon,
+   FilesAndFoldersIcon,
+   GraphicIcon,
+   LayoutIcon,
+   PeopleIcon,
+   ToolsIcon,
+   ArchiveIcon,
+   ArrowsSystemIcon,
+   AttachIcon,
+   CalendarIcon,
+   CheckIcon,
+   ClockIcon,
+   CopyIcon,
+   DeleteIcon,
+   DoneIcon,
+   EditIcon,
+   ExitIcon,
+   HideIcon,
+   LabelIcon,
+   MemberIcon,
+   MinusIcon,
+   NotificationIcon,
+   NotifyIcon,
+   PlusIcon,
+   SearchIcon,
+   ShowIcon,
+   StarIcon,
+   TemplateIcon,
+}
