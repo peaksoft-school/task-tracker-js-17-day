5bd890e1730cb7d516eda8f7c71b1cab1fc5ee47
Nursultan Kenzhebaev <dvaraz134@gmail.com>
2025-07-29 17:22:05 +0600

    exchange code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Input } from './components/UI/input/Input'
+import { PeopleIcon } from './assets/AllExportIcon'
 
 export default function App() {
    return (
-      <div>
+      <div style={{ width: 100 }}>
          <h1>hello</h1>
+         <Input />
       </div>
    )
 }
