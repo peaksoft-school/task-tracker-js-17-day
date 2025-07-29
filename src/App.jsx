de8566e34445dc2b817d1a47602fe4b7983c596a
Nursultan Kenzhebaev <dvaraz134@gmail.com>
2025-07-29 17:23:35 +0600

    clear app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { Input } from './components/UI/input/Input'
-import { PeopleIcon } from './assets/AllExportIcon'
 
 export default function App() {
    return (
-      <div style={{ width: 100 }}>
+      <div>
          <h1>hello</h1>
-         <Input />
       </div>
    )
 }
