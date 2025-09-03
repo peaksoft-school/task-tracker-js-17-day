74ac78482bd8abfd96cce4cf20e61c52e97fcf56
Kudaibergen <kudash1525@gmail.com>
2025-09-03 10:26:43 +0600

    убрал свои черрновики

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Sidebar from './components/sidebar/Sidebar'
-import TemporaryDrawer from './Test'
 import './assets/fonts/fonts.css'
-import Sidebar_2_ from './components/sidebar/Sidemar_2_'
+import Sidebar from './components/sidebar/Sidebar'
 
 export default function App() {
    return (
       <div>
-         {/* <TemporaryDrawer /> */}
          <Sidebar />
-         {/* <Sidebar_2_ /> */}
       </div>
    )
 }
