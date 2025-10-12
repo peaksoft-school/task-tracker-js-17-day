a25d006ddbc1a94dc6a5a32d682f14e0aa7438c5
Rabiya <rabiyaaiylchieva@gmail.com>
2025-10-12 23:39:26 +0600

    fixed all

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import DatePicker from './layouts/date-picker/DatePicker'
-import {  Selectt } from './components/select/Selectt'
-import Chipp from './components/chip/Chip'
-
-function App() {
-   return <h1>Task Tracer</h1>
-}
+const App = () => <h1>Task Tracer</h1>
 
 export default App
--- src/components/chip/Chip.jsx
+++ src/components/chip/Chip.jsx
@@ -1,109 +1,75 @@
-import { Box, Chip, Stack, styled } from '@mui/material'
-import React from 'react'
-import { ExitIcon, PlusIcon, WhitePliusIcon } from '../../assets/AllExportIcon'
+import { Chip, styled } from '@mui/material'
+import { ExitIcon } from '../../assets/AllExportIcon'
 
-export default function Chipp() {
-   const handleClick = () => {
-      console.info('You clicked the Chip.')
-   }
+export const Chip = ({
+   label,
+   color = 'gray',
+   icon,
+   isRound = false,
+   onClick,
+   removable = false,
+   onDelete,
+}) => (
+   <StyledChip
+      label={
+         <ChipContent>
+            {icon && <IconWrapper>{icon}</IconWrapper>}
+            <span>{label}</span>
+            {removable && (
+               <ExitIconWrapper
+                  onClick={(e) => {
+                     e.stopPropagation()
+                     onDelete && onDelete()
+                  }}
+               >
+                  <ExitIcon />
+               </ExitIconWrapper>
+            )}
+         </ChipContent>
+      }
+      onClick={onClick}
+      colorProp={color}
+      isRound={isRound}
+   />
+)
 
-   const handleDelete = () => {
-      console.info('You clicked the delete icon.')
-   }
-
-   return (
-      <StyledBox>
-         <StyledStack direction="row" spacing={1}>
-            <StylesChip
-               colors="greny"
-               label={
-                  <StyledSpan>
-                     Done
-                     <StyledExitIcon />
-                  </StyledSpan>
-               }
-               onClick={handleClick}
-            />
-            <StylesChip
-               colors="red"
-               label={
-                  <StyledSpan>
-                     The task is burning
-                     <StyledExitIcon />
-                  </StyledSpan>
-               }
-               onClick={handleClick}
-            />
-            <StylesChip
-               colors="orange"
-               label={
-                  <StyledSpan>
-                     Pay attention to task 1
-                     <StyledExitIcon />
-                  </StyledSpan>
-               }
-               onClick={handleClick}
-            />
-            <StyledChipPlus
-               label={
-                  <StyledSpan>
-                     <StyledExitIconPlus />
-                  </StyledSpan>
-               }
-               onClick={handleClick}
-            />
-         </StyledStack>
-      </StyledBox>
-   )
-}
-
-const StyledBox = styled(Box)({
-   padding: '20px',
-   width: '520px',
-   height: '30px',
-})
-
-const StyledStack = styled(Stack)({
-   display: 'flex',
-   alignItems: 'center',
-})
-
-const StylesChip = styled(Chip)(({ colors }) => ({
-   maxwidth: '100%',
-   maxHeight: '100]',
-   padding: '10px',
-   borderRadius: '6px',
-   color: 'white',
-   fontWeight: '500',
-   fontSize: '16px',
+const StyledChip = styled(Chip, {
+   shouldForwardProp: (prop) => prop !== 'colorProp' && prop !== 'isRound',
+})(({ colorProp, isRound }) => ({
+   borderRadius: isRound ? '50%' : '6px',
+   width: isRound ? '40px' : 'auto',
+   height: isRound ? '40px' : 'auto',
+   padding: isRound ? '0' : '10px',
    backgroundColor:
-      colors === 'greny'
-         ? 'rgb(48, 176, 9)'
-         : colors === 'red'
-           ? 'rgb(217, 21, 17)'
-           : colors === 'orange'
-             ? 'rgb(237, 138, 0)'
-             : '#rgb(201, 201, 201)',
-}))
-
-const StyledChipPlus = styled(Chip)(({}) => ({
-   borderRadius: '100%',
-   width: '40px',
-   height: '40px',
-   backgroundColor: 'rgb(201, 201, 201)',
-}))
-
-const StyledExitIconPlus = styled(WhitePliusIcon)({
-   width: '19px',
-   height: '19px',
-})
-
-const StyledExitIcon = styled(ExitIcon)({
-   opacity: 2,
-})
-
-const StyledSpan = styled('span')({
+      colorProp === 'green'
+         ? 'rgb(48,176,9)'
+         : colorProp === 'red'
+           ? 'rgb(217,21,17)'
+           : colorProp === 'orange'
+             ? 'rgb(237,138,0)'
+             : 'rgb(201,201,201)',
+   color: 'white',
+   fontWeight: 500,
+   fontSize: 16,
    display: 'flex',
    alignItems: 'center',
-   gap: '6px',
+   justifyContent: 'center',
+}))
+
+const ChipContent = styled('span')({
+   display: 'flex',
+   alignItems: 'center',
+   gap: 6,
+})
+
+const IconWrapper = styled('span')({
+   display: 'flex',
+   alignItems: 'center',
+   '& svg': { width: 19, height: 19 },
+})
+
+const ExitIconWrapper = styled('span')({
+   display: 'flex',
+   alignItems: 'center',
+   cursor: 'pointer',
 })
