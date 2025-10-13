614690d88c82e919459ed7657c68356dd203dea3
Kudaibergen <kudash1525@gmail.com>
2025-10-13 22:58:53 +0600

    open fail Issues.jsx

---
+++ src/pages/Issues.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../layouts/header/Header'
+
+export default function Issues() {
+   return (
+      <>
+         <Header />
+         <h1>hello</h1>
+      </>
+   )
+}
