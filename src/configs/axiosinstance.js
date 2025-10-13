b817f59369f180c91cfc3899236010d5c9b49b16
Kudaibergen <kudash1525@gmail.com>
2025-10-13 19:20:44 +0600

    пол пути main

--- src/configs/axiosinstance.js
+++ src/configs/axiosinstance.js
@@ -5,7 +5,7 @@ const BASE_URL = 'http://3.75.184.75'
 export const axiosInstance = axios.create({
    baseURL: BASE_URL,
 
-   heders: {
+   headers: {
       'Content-Type': 'application/json',
    },
 })
