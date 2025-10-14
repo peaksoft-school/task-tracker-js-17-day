f3845bd1cb416f9caac2172ba41b7336f6e37e7d
Rabiya <rabiyaaiylchieva@gmail.com>
2025-10-14 17:22:45 +0600

    clear app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/AppRoutes'
-import { Header } from './layouts/header/Header'
 
 const App = () => {
-   // return <RouterProvider router={routes} />
-   return <Header />
+   return <RouterProvider router={routes} />
 }
 
 export default App
