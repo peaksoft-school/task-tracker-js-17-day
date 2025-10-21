a8b384cce077207792fc0c439eef50e6b31c1190
Kudaibergen <kudash1525@gmail.com>
2025-10-21 22:02:33 +0600

    issuse вёрстка без запроса

---
+++ src/assets/backgroundImg/backgroundColors.js
@@ -0,0 +1,8 @@
+export const Colors = [
+   { color: '#cccccc' },
+   { color: '#b04631' },
+   { color: '#3e702d' },
+   { color: '#d19034' },
+   { color: '#895f9e' },
+   { color: '#015d91' },
+]
