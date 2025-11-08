086a59200c55e7197d5de18498a76d087d91fa89
Kudaibergen <kudash1525@gmail.com>
2025-11-08 18:47:07 +0600

    просто преместил из ветки feature/issues папку sidebar в ветку feature/Sidebar

---
+++ src/components/sidebar/SidebarItem.jsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled'
+
+function SidebarItem({ icon, label, count, open, isActive, onClick }) {
+   return (
+      <ItemContainer open={open} isActive={isActive} onClick={onClick}>
+         <IconWrapper>{icon}</IconWrapper>
+
+         {open && (
+            <>
+               <LabelText>{label}</LabelText>
+               {count && <CountText>{count}</CountText>}
+            </>
+         )}
+      </ItemContainer>
+   )
+}
+
+export default SidebarItem
+
+const ItemContainer = styled('div')(({ open, isActive }) => ({
+   display: 'flex',
+   alignItems: 'center',
+   width: open && isActive ? 227 : undefined,
+   background: open && isActive ? 'rgba(58,104,131,0.6)' : undefined,
+   borderRadius: open && isActive ? '0 24px 24px 0' : undefined,
+   padding: open ? '8px 32px 9px 43px' : undefined,
+   cursor: 'pointer',
+}))
+
+const CountText = styled('span')({ marginLeft: 6 })
+
+const IconWrapper = styled('div')({
+   display: 'flex',
+   alignItems: 'center',
+   '& svg': { width: 20, height: 20 },
+})
+
+const LabelText = styled('span')({ marginLeft: 12 })
